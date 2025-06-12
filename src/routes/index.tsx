8a699fb14478b3e3f35ebff18c7663912d1be23d
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-06-12 23:36:29 +0530

    Implement product loader in Index route to fetch and display product details, enhancing the main entry point with dynamic data rendering.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,28 @@
+import {getAllProducts} from "../api/inventory-service.ts";
+import type {Product} from "../types/product.ts";
+
 export const Route = createFileRoute({
-    component: Index
+    component: Index,
+    loader: async () => {
+       const products : Product[] = await getAllProducts(["name","stock"]);
+        console.log(products)
+        return {products}
+    },
+    errorComponent: ( {error} ) => <div>something went wrong. {error.message}</div>
 })
 
 function Index ()  {
+    const { products } = Route.useLoaderData();
     return (
-        <div>Index</div>
+        <div>
+            {products.map((item) => (
+                <div key={item.id}>
+                    <h2>Name : {item.name}</h2>
+                    <h4>price: {item.stock}</h4>
+                    <h4>price: {item.price}</h4>
+                </div>
+            ))}
+        </div>
     )
 }
 
