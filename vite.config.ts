6176e81c5d9c6afd44f99e3b1eca95d639c10cd0
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-06-12 11:46:44 +0530

    Add Tailwind CSS plugin to Vite configuration for improved styling support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tanstackRouter from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,5 @@ export default defineConfig({
     autoCodeSplitting: true,
     verboseFileRoutes: false,
   }),
-    react()],
+    react(),tailwindcss(),],
 })
