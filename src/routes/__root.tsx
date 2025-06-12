2d8f7eae3ece94400c094637baa6339015901840
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-06-12 11:19:03 +0530

    Add root route component with navigation links and TanStack Router Devtools

---
+++ src/routes/__root.tsx
@@ -0,0 +1,21 @@
+import {createRootRoute, Link, Outlet} from "@tanstack/react-router";
+import {TanStackRouterDevtools} from "@tanstack/react-router-devtools";
+
+
+export const Route = createRootRoute({
+    component: () => (
+        <>
+            <div>
+                <Link to={"/"}>
+                    Home
+                </Link>
+                <Link to={"/products/update"}>
+                    update
+                </Link>
+            </div>
+            <hr/>
+            <Outlet/>
+            <TanStackRouterDevtools/>
+        </>
+    )
+})
