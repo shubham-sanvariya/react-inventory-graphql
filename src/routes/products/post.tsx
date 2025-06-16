f7a3d49a774040c66e79755968aea6ff4de80bbd
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-06-16 12:19:29 +0530

    Implement product submission logic in Post component, integrating useProductMutation for handling form submission, validation, and error display. Update form behavior to reset after submission and conditionally render loading state.

--- src/routes/products/post.tsx
+++ src/routes/products/post.tsx
@@ -4,12 +4,14 @@ import {
     Initial_Product_Values,
     postProductFormKeys
 } from "../../validator/postProductValidator.ts";
+import {useProductMutation} from "../../mutation/productMutation.ts";
 
 export const Route = createFileRoute({
     component: Post
 })
 
 function Post() {
+    const {mutate, isPending, isError, error} = useProductMutation();
 
     const postProductForm = useForm({
         defaultValues: Initial_Product_Values,
@@ -17,17 +19,21 @@ function Post() {
             onChange: createProductPostSchema
         },
         asyncDebounceMs: 300,
-        onSubmit: async () => {
+        onSubmit: () => {
+            const product = postProductForm.state.values;
+            mutate(product);
+            postProductForm.reset();
+        },
 
-        }
     })
 
     return (
         <div className={'flex flex-col justify-center items-center p-2 m-2'}>
             <h1>Add a Product</h1>
-            <form onSubmit={(e) => {
-                e.preventDefault()
-                postProductForm.handleSubmit()
+            <form onSubmit={async (e) => {
+                e.preventDefault();
+                postProductForm.validate('submit');
+                await postProductForm.handleSubmit()
             }} id={"product-form"} className={'flex flex-col items-center'}>
                 {postProductFormKeys.map((item, index) => (
                     <postProductForm.Field name={item[0]} key={index}>
@@ -40,11 +46,17 @@ function Post() {
                                         id={item[0]}
                                         name={item[0]}
                                         type={item[1]}
-                                        onChange={(e) => field.handleChange(e.target.value)}
+                                        onChange={(e) => {
+                                            if (item[1] === "string") {
+                                                field.handleChange(e.target.value)
+                                            } else {
+                                                field.handleChange(Number(e.target.value))
+                                            }
+                                        }}
                                         placeholder={item[2]}
                                     />
                                 </div>
-                                {(field.state.meta.isTouched || postProductForm.state.isSubmitted) && Array.isArray(field.state.meta.errors) && field.state.meta.errors.length > 0 &&(
+                                {(field.state.meta.isTouched || postProductForm.state.isSubmitted) && Array.isArray(field.state.meta.errors) && field.state.meta.errors.length > 0 && (
                                     <div className={'text-red-500'}>
                                         {typeof field.state.meta.errors[0] === 'object' && 'message' in field.state.meta.errors[0]
                                             ? field.state.meta.errors[0].message
@@ -55,8 +67,15 @@ function Post() {
                         )}
                     </postProductForm.Field>
                 ))}
-                <button className={'border-1 mt-2 text-center w-20 bg-cyan-100'} type="submit" form={"product-form"} disabled={postProductForm.state.isSubmitting}>Post</button>
+                <button className={'border-1 mt-2 text-center w-20 bg-cyan-100'} type="submit"
+                        disabled={postProductForm.state.isSubmitting || !postProductForm.state.isFormValid}>
+
+                    {isPending ? "adding..." : "Add"}
+                </button>
             </form>
+            {
+                isError && <div className="error">{error.message}</div>
+            }
         </div>
     )
 }
